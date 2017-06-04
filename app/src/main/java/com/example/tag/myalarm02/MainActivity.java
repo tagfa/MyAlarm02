d3e9ad06d8daabefdee7dec393b3674c7c5e2b31
tagfa <tagfa.job@gmail.com>
2017-06-04 19:50:09 +0900

    test

--- app/src/main/java/com/example/tag/myalarm02/MainActivity.java
+++ app/src/main/java/com/example/tag/myalarm02/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity
     TextView textView;
     Calendar calendar;
 
+    //test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
